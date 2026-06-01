b18ded3f1ae961d480537b8e3deceefefcbaffe8
rajendra <ghugerajendra358@gmail.com>
2026-06-01 23:00:47 +0530

    Update vidzee stream parameters

--- src/api.js
+++ src/api.js
@@ -148,7 +148,7 @@ export const movieApi = {
                 10: "https://vidsrc.wtf/api/3/movie/?id={id}&autoplay=1",
                 11: "https://peachify.top/embed/movie/{id}?dub=Hindi&sub=English",
                 12: "https://111movies.com/movie/{id}",
-                13: "https://player.vidzee.wtf/embed/movie/{id}?sr=hindi&server=7&autoplay=true"
+                13: "https://player.vidzee.wtf/embed/movie/{id}?sr=7&autoplay=true"
             },
             tv: {
                 5: "https://vidlux.site/embed/tv/{id}/{s}/{e}",
@@ -163,7 +163,7 @@ export const movieApi = {
                 10: "https://vidsrc.wtf/api/3/tv/?id={id}&s={s}&e={e}&autoplay=1",
                 11: "https://peachify.top/embed/tv/{id}/{s}/{e}?dub=Hindi&sub=English",
                 12: "https://111movies.com/tv/{id}/{s}/{e}",
-                13:"https://player.vidzee.wtf/embed/tv/{id}/{s}/{e}?sr=hindi&server=7&autoplay=true"
+                13:"https://player.vidzee.wtf/embed/tv/{id}/{s}/{e}?sr=7&autoplay=true"
             }
         };
 
